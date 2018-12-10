13de90b3955eedb654cdb2a6df46477fad1246e9
Ponton 0 <rarataxy@gmail.com>
2018-12-10 16:04:44 +0100

    added a weather randomizing function

--- functions.js
+++ functions.js
@@ -1,7 +1,20 @@
+const weatherData = require("./weatherData.json");
+const weather = require("./weather.json")
+const weatherStatus = require("./weatherStatus.json");
+const fs = require("fs");
+const weatherupdater = require("./weatherupdater.js");
+
+
 module.exports = {
  colors: function colors(){
     let kolors = ['#b53000','#e0af00','#b1e800','#1daa11','#09b774','#1092ba','#262da8','#7228a3','#bf1cae','#d30860','#c41717'];
     let random = kolors[Math.floor(Math.random()*kolors.length)];
     return random;
+  },
+
+ randomweather: function randomweather(){
+    const weathers = [weatherupdater.cloudy, weatherupdater.rainy, weatherupdater.sunny, weatherupdater.snowy, weatherupdater.stormy, weatherupdater.windy]
+    var rand = weathers[Math.floor(Math.random() * weathers.length)];
+    (rand)()
   }
 }
